9afbc510d8ab995ed457c4ffbe849f1ca75e813d
Peggy Carambas <PCarambas@gmail.com>
2022-10-07 10:30:58 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,60 @@
+/*  Pick shirt pattern
+Choose correct size
+Read pattern directions
+Gather materials
+ Choose my fabric
+ Cut pattern using projector
+ Sew pattern together
+ Check fit of shirt */
+
+//  Write function for each task
+//  Inside each function body, console.log description of each task
+//  Invoke each task in order
+
+const pickShirtPattern = () => {
+    console.log('Which stylish shirt should we sew today?')
+}
+
+const choosePatternSize = () => {
+    console.log('Measure chest and waist.')
+
+}
+
+const readDirections = () => {
+    console.log('Look over each step')
+
+}
+
+const gatherMaterials = () => {
+    console.log('Collect thread, rotary cutter, snips, pins and turn on sewing machine.')
+
+}
+
+const chooseFabric = () => {
+    console.log('Choose fabric based on pattern requirements and my likes.')
+
+}
+
+const cutPattern = () => {
+    console.log('Use rotary cutter to cut pattern pieces.')
+
+}
+
+const sewPatternTogether = () => {
+    console.log('Follow steps in pattern directions.')
+
+}
+
+const checkFit = () => {
+    console.log('Try on my new shirt.')
+
+}
+
+pickShirtPattern()
+choosePatternSize()
+readDirections()
+gatherMaterials()
+chooseFabric()
+cutPattern()
+sewPatternTogether()
+checkFit()
